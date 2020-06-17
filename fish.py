d66fab7eee6c686e72723e0964854a9b4549296b
Audrey Andoy <audreylandoy@gmail.com>
2020-06-17 18:15:43 +0000

    added fish class, made multiple fish appear and move, and used key/click events to delete and add fishes

---
+++ fish.py
@@ -0,0 +1,37 @@
+import pygame, random
+
+class Fish:
+    def __init__(self, pos):
+        self.image = pygame.image.load("fish.png")
+        scale = random.randint(1,5)*10
+        self.image = pygame.transform.smoothscale(self.image, (scale, scale))
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+        self.speed = pygame.math.Vector2(0, random.randint(2,5))
+
+        rotation = random.randint(0, 360)
+        self.speed.rotate_ip(rotation)
+        self.image = pygame.transform.rotate(self.image, 180- rotation)
+
+    def update(self):
+        # Move the fish: randomly, diagonally, bounces, and goes the opposite direction
+        screen_info = pygame.display.Info()
+        self.rect.move_ip(self.speed)
+        # if fish touches left or right edge of screen
+        if self.rect.left < 0 or self.rect.right > screen_info.current_w:
+            self.speed[0] *= -1
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.rect.move_ip(self.speed[0], 0)
+        # if fish touches top or bottom edge of screen
+        if self.rect.top < 0 or self.rect.bottom > screen_info.current_h:
+            self.speed[1] *= -1
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.rect.move_ip(0, self.speed[1])
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+import random
 from pygame.locals import *
 import sys
 
+from fish import *
 
 pygame.init()
 screen_info = pygame.display.Info()
@@ -11,13 +13,28 @@ screen_size = (width, height) = (int(screen_info.current_w), int(screen_info.cur
 screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
 
-
 color = (0, 127, 255)
+school = []
+
 
 def main():
+    for i in range(10):
+        school.append(Fish((width/2, height/2)))
+   
     while True:
         clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                school.append(Fish(event.pos))
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    for i in range(len(school) // 2 ):
+                        school.pop(0)
         screen.fill(color)
+        for x in school:
+            x.update()
+            x.draw(screen)
+
         pygame.display.flip()
 
 if __name__ == '__main__':
